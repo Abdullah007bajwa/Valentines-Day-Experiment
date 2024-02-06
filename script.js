fb2393f3f6d8c47001143e44bd89901a9c6e5ede
Abdullah007bajwa <bajwa15523@gmail.com>
2024-02-06 23:58:23 +0500

    A

--- script.js
+++ script.js
@@ -17,30 +17,29 @@ function handleYesClick() {
       modal.remove();
     });
   }
+  function handleNoClick() {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    const noButtonText = phrases[randomIndex];
+    
+    document.querySelector('.no-button').textContent = noButtonText;
+  }
   
-
-let noCount = 0;
-function handleNoClick() {
-  const phrases = [
-    "No",
-    "Are you sure?",
-    "Really sure?",
-    "Think again!",
-    "Last chance!",
-    "Surely not?",
-    "You might regret this!",
-    "Give it another thought!",
-    "Are you absolutely certain?",
-    "This could be a mistake!",
-    "Have a heart!",
-    "Don't be so cold!",
-    "Change of heart?",
-    "Wouldn't you reconsider?",
-    "Is that your final answer?",
-    "You're breaking my heart ;(",
-  ];
-
-  const noButtonText = phrases[Math.min(noCount, phrases.length - 1)];
-  document.querySelector('.no-button').textContent = noButtonText;
-  noCount++;
-}
